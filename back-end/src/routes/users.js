f57a61ceaf0fbbe7a6457e897c78e8ce8ed796de
diyagr <dpa9013@nyu.edu>
2024-12-01 18:28:31 -0500

    Updated users.js file

--- back-end/src/routes/users.js
+++ back-end/src/routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+import { verifyToken } from '../controllers/authMiddleware.js';
 const router = express.Router();
 const {
   getProfile,
@@ -12,30 +13,30 @@ const {
  * @route GET /api/users/:userId/profile
  * @description Retrieves the profile information for a specified user.
  */
-router.get('/:userId/profile', getProfile);
+router.get('/:userId/profile', verifyToken, getProfile);
 
 /**
  * @route POST /api/users/:userId/profile
  * @description Saves the profile information for a specified user.
  */
- router.post('/:userId/profile', saveProfile); 
+ router.post('/:userId/profile', verifyToken, saveProfile); 
 
 /**
  * @route GET /api/users/:userId/saved-routes
  * @description Fetches all saved routes for a specified user.
  */
-router.get('/:userId/saved-routes', getSavedRoutes);
+router.get('/:userId/saved-routes', verifyToken, getSavedRoutes);
 
 /**
  * @route POST /api/users/:userId/saved-routes
  * @description Adds a new saved route for a specified user.
  */
-router.post('/:userId/saved-routes', addSavedRoute);
+router.post('/:userId/saved-routes', verifyToken, addSavedRoute);
 
 /**
  * @route DELETE /api/users/:userId/saved-routes/:routeId
  * @description Deletes a specified saved route for a user.
  */
-router.delete('/:userId/saved-routes/:routeId', deleteSavedRoute);
+router.delete('/:userId/saved-routes/:routeId', verifyToken, deleteSavedRoute);
 
 module.exports = router;
